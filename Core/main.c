41bfe725303539017541dad66cfb041ef00dab5b
trembach <trembach.d@gmail.com>
2020-08-03 19:08:58 +0300

    Шаблон стартового проекта STM32H743ZI.
      HAL v1.70, FreeRTOSv10.3.1, модуль отладки cmd_hdl.
    
    Signed-off-by: trembach <trembach.d@gmail.com>

---
+++ Core/main.c
@@ -0,0 +1,105 @@
+/**
+  ******************************************************************************
+  * @file    main.c 
+  * @author  Trembach D.N.
+  * @version V1.0.0
+  * @date    23-08-2020
+  * @brief   
+  *
+  *
+  ******************************************************************************
+  * @attention
+  *
+  *
+  * <h2><center>&copy; COPYRIGHT 2020 OneTiOne </center></h2>
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* Kernel includes. */
+#include "printf_dbg.h"
+#include "cmd_process.h"
+
+volatile const char __version__[] = "NUCLEO-H743ZI";    
+volatile const char __date__[] = __DATE__;
+volatile const char __time__[] = __TIME__;
+
+/**
+ * @brief  Start Thread 
+ * @param  None
+ * @retval None
+ */
+void system_thread(void *arg)
+{ 
+	//Подключение интерфейса отладки
+	DBG_Hardware_Setup();
+
+	/* init code for LWIP */
+	//LWIP_Init();	
+		
+	//Инициализация задачи диагностического терминала 
+	xTaskCreate(terminal_task, (const char*)"CmdTrmnl", configMINIMAL_STACK_SIZE * 5, NULL, TreadPrioNormal, NULL);
+	
+	// Информационная шапка программы
+	printf("______________________________________________\r\n");
+	printf("\r\n");
+	printf("   %s \r\n", __version__);
+	printf("   DATA: %s \r\n", __date__);
+	printf("   TIME: %s \r\n", __time__);
+	printf("   CPU FREQ = %.9lu Hz \r\n", SystemCoreClock);  
+	printf("______________________________________________\r\n"); 
+  
+	
+	for (;;) {
+		vTaskDelay(500);
+	}
+}
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+	/* MCU Configuration--------------------------------------------------------*/
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+	
+	/* Init thread */
+	xTaskCreate(system_thread, (const char*)"SysTask", configMINIMAL_STACK_SIZE * 2, NULL, TreadPrioNormal, NULL);
+	
+	/* Start scheduler */
+	vTaskStartScheduler();
+  
+	/* We should never get here as control is now taken by the scheduler */
+	NVIC_SystemReset();
+	return 0;
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+	/* User can add his own implementation to report the HAL error return state */
+	while (1) ;
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+	/* User can add his own implementation to report the file name and line number,
+	   tex:  */
+	printf("Wrong parameters value: file %s on line %lu\r\n", file, line);
+	while (1) ;
+}
+#endif /* USE_FULL_ASSERT */
+/************************ COPYRIGHT 2020 OneTiOne *****END OF FILE****/
---
+++ Core/stm32h7xx_hal_msp.c
@@ -0,0 +1,113 @@
+/**
+  ******************************************************************************
+  * @file       stm32h7xx_hal_msp.c
+  * @author     Trembach D.N.
+  * @version    V1.5.0
+  * @date       25-02-2020
+  * @brief      Файл инициализации HAL.
+  *
+  *
+  ******************************************************************************
+  * @attention
+  *
+  *
+  * <h2><center>&copy; COPYRIGHT 2020 OneTiOne </center></h2>
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = { 0 };
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	
+	/** Supply configuration update enable 
+	*/
+	HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+	/** Configure the main internal regulator output voltage 
+	*/
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+	/** Macro to configure the PLL clock source 
+	*/
+	__HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSE);
+	/** Initializes the CPU, AHB and APB busses clocks 
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 1;
+	RCC_OscInitStruct.PLL.PLLN = 120;
+	RCC_OscInitStruct.PLL.PLLP = 2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+	RCC_OscInitStruct.PLL.PLLFRACN = 0;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB busses clocks 
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+	                            | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+	                            | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+	RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_USB;
+	PeriphClkInitStruct.PLL3.PLL3M = 1;
+	PeriphClkInitStruct.PLL3.PLL3N = 48;
+	PeriphClkInitStruct.PLL3.PLL3P = 2;
+	PeriphClkInitStruct.PLL3.PLL3Q = 8;
+	PeriphClkInitStruct.PLL3.PLL3R = 2;
+	PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_3;
+	PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
+	PeriphClkInitStruct.PLL3.PLL3FRACN = 0;
+
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3;
+	PeriphClkInitStruct.Usart234578ClockSelection = RCC_USART234578CLKSOURCE_D2PCLK1;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/**
+  * Initializes the Global MSP.
+  */
+void HAL_MspInit(void)
+{
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+
+	/* System interrupt init*/
+	/* PendSV_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
+
+	/* Configure the system clock */
+	SystemClock_Config();
+	SystemCoreClockUpdate();
+}
+/************************ COPYRIGHT 2020 OneTiOne *****END OF FILE****/
