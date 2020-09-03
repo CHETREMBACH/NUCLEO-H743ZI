66b57bc2692455eb5a9cba647ce0e7c71c043e5e
trembach <trembach.d@gmail.com>
2020-09-03 22:41:29 +0300

    Доработан файл скрипта линкера, основная оперативная память вынесена из блока где не работает DMA.
       Изменена структура папок проекта.
    
       Удалены лишние файлы и настройки.
    
    Signed-off-by: trembach <trembach.d@gmail.com>

--- Config/FreeRTOSConfig.h
+++ Config/FreeRTOSConfig.h
@@ -60,7 +60,7 @@
 #define configUSE_TICKLESS_IDLE                         0
 #define configCPU_CLOCK_HZ                              ( SystemCoreClock )
 #define configTICK_RATE_HZ                              ((TickType_t)1000)
-#define configMAX_PRIORITIES                            ( 32 )
+#define configMAX_PRIORITIES                            ( 7 )
 #define configMINIMAL_STACK_SIZE                        ((uint16_t)128)
 #define configMAX_TASK_NAME_LEN                         ( 16 )
 #define configUSE_16_BIT_TICKS			                0       
