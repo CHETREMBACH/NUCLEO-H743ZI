9596f46855dbed8b32390c23ee1d2fe79dd9d4ee
trembach <trembach.d@gmail.com>
2020-09-03 22:41:29 +0300

    Доработан файл скрипта линкера, основная оперативная память вынесена из блока где не работает DMA.
       Изменена структура папок проекта.
    
       Удалены лишние файлы и настройки.
    
    Signed-off-by: trembach <trembach.d@gmail.com>

--- Core/Scr/hooks.c
+++ Core/Scr/hooks.c
@@ -27,8 +27,31 @@ static volatile unsigned int mallocFailed;
 /* Указать размешение heap */
 #if ( configAPPLICATION_ALLOCATED_HEAP == 1 )
 /* Allocate the memory for the heap. */
-uint8_t __attribute__((section(".RTOS_HEAP_RAM_D3"))) ucHeap[configTOTAL_HEAP_SIZE];	
+uint8_t __attribute__((section(".RTOS_HEAP_ITCMRAM"))) ucHeap[configTOTAL_HEAP_SIZE];	
 #endif /* configAPPLICATION_ALLOCATED_HEAP */
+
+ void vApplicationIdleHook(void)
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	to 1 in FreeRTOSConfig.h. It will be called on each iteration of the idle
+	task. It is essential that code added to this hook function never attempts
+	to block in any way (for example, call xQueueReceive() with a block time
+	specified, or call vTaskDelay()). If the application makes use of the
+	vTaskDelete() API function (as this demo application does) then it is also
+	important that vApplicationIdleHook() is permitted to return to its calling
+	function, because it is the responsibility of the idle task to clean up
+	memory allocated by the kernel to any task that has since been deleted. */
+}
+
+void vApplicationTickHook(void)
+{
+	/* This function will be called by each tick interrupt if
+	configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h. User code can be
+	added here, but the tick hook is called from an interrupt context, so
+	code must not attempt to block, and only the interrupt safe FreeRTOS API
+	functions can be used (those that end in FromISR()). */
+}
+
 
 /**
   * @brief  Функция вызывается при переполнении стека FreeRTOS, 
@@ -39,9 +62,12 @@ uint8_t __attribute__((section(".RTOS_HEAP_RAM_D3"))) ucHeap[configTOTAL_HEAP_SI
 void vApplicationStackOverflowHook(TaskHandle_t xTask,
                                    signed portCHAR *pcTaskName)
 {
+/* Run time stack overflow checking is performed if
+configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2. This hook function is
+called if a stack overflow is detected. */
 	stackOverflow++;
 	printf("StackOverflowHook: %s\n", pcTaskName);
-    while(1);
+	while (1) ;
 }
 
 /**
